d427d025e7e2e3c65654412306a4f74783005f81
Aizen sama <156284808+Mahesh8899p@users.noreply.github.com>
2026-07-22 21:45:43 +0530

    changes

---
+++ arrayeasy/rearrangearrayelementsbysize.java
@@ -0,0 +1,18 @@
+package arrayeasy;
+
+public class rearrangearrayelementsbysize {
+    public void swap(int[] nums, int i,int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static int[] rearrangearray(int[] nums){
+        
+        int i =0;
+        int j = i+1;
+        while(i<j){
+            if(arr[])
+        }
+    }
+}
