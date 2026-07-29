dc1680d4e3f49dc07f373a2733b748ca224f89e9
Mahesh8899p <mohit8mahesh.7@gmail.com>
2026-07-29 09:36:44 +0530

    Refactor displayleaders method to correctly identify and add leaders to the result list

--- arraystrv/leadersofarray.java
+++ arraystrv/leadersofarray.java
@@ -1,8 +1,30 @@
 package arraystrv;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
 public class leadersofarray{
-
+    //[1, 2, 5, 3, 1, 2]
     public static List<Integer> displayleaders(int[] nums){
-        
+        List<Integer> result = new ArrayList<>();
+
+        int n = nums.length;
+        int maxSoFar = nums[n-1];//last element is always a leader
+        result.add(maxSoFar);//adding the last element in the List
+        for(int i = n-2;i>=0;i--){//for loop starting from n-2 
+           if(nums[i] > maxSoFar){
+            maxSoFar = nums[i];
+            result.add(maxSoFar);
+
+           }
+        }
+        Collections.reverse(result);//collections has one reverse method which can be used to reverse the different elements in the list
+        return result;
+    }
+    
+    public static void main(String[] args){
+        int[] nums = {16, 17, 4, 3, 5, 2};
+        List<Integer> leaders = displayleaders(nums);
+        System.out.println(leaders);
     }
 }
