dad0f72861831758305c4630dd95c364df8ed30e
Mahesh <mohit8mahesh.7@gmail.com>
2026-08-28 13:56:03 +0530

    Refactor spiralOrder method: streamline logic for traversing matrix in spiral order

--- arraystrv/practice.java
+++ arraystrv/practice.java
@@ -1,17 +1,52 @@
 package arraystrv;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 public class practice{
       public List<Integer> spiralOrder(int[][] matrix){
-            int n = matrix[0].length;
-            int i = 0;
-            int j = n -1;
-            
+       int rowbegin = 0;
+       int rowend = matrix.length-1;
+       int colstart = 0;
+       int colend = matrix[0].length-1;
+       int n = matrix.length;
+       ArrayList<Integer> list = new ArrayList<>();
+
+
+       while(rowbegin <= rowend && colstart <= colend){
+
+            //print the first row from left to right
+       for(int j = colstart;j<=colend;j++){
+            list.add(matrix[rowbegin][j]);
+       } 
+       rowbegin++;
+       
+
+       //print from top to bottom
+       for(int i = rowbegin; i<=rowend;i++){
+            list.add(matrix[i][colend]);
+       }
+       colend--;
+
+       //print from right to left
+       if(rowbegin <= rowend){
+            for(int i = colend;i>=colstart;i--){
+                  list.add(matrix[rowend][i]);
+            }
+            rowend--;
+       }
+       
+
+       //print from top to bottom
+
+       if(colstart <= colend){
+            for(int i= rowend;i>=rowbegin;i--){
+                  list.add(matrix[i][colstart]);
+            }
+            colstart++;
+       }         
 
       }
-   
+      return list;
 }
-
-      
+}  
 
 
